2a7feb0155239ad387a8e8b4fbb009ee8e69de95
deljus <musindelus@gmail.com>
2019-03-12 22:28:11 +0300

    убраны не исспользуемые компоненты

--- dynamic/components/index.js
+++ dynamic/components/index.js
@@ -1,12 +1,10 @@
 import Alert from './Alerts';
-import MaskInputControl from './MaskedInput';
 import SelectControl from './SelectControl';
 import AutocompliteInput from './AutocompliteInput';
 import HtmlEditor from './HtmlEditor';
 
 export {
   Alert,
-  MaskInputControl,
   SelectControl,
   AutocompliteInput,
   HtmlEditor
